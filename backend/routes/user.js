aea142583ad93ebf6201698bbaf31311d8c27706
leopassosleite <leopassos.leite4@gmail.com>
2022-05-19 07:17:54 -0300

    Implementa login

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,6 +2,9 @@ const express = require('express');
 const connection = require('../connection');
 const router = express.Router();
 
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
 router.post('/signup', (req, res) => {
     let user = req.body;
     query = "select email,password,role,status from user where email=?"
@@ -28,5 +31,31 @@ router.post('/signup', (req, res) => {
     })
 })
 
+router.post('/login', (req, res) => {
+    const user = req.body;
+    query = "select email,password,role,status from user where email=?"
+    connection.query(query, [user.email], (err, results) => {
+        if (!err) {
+            if (results.length <= 0 || results[0].password != user.password) {
+                return res.status(401).json({ message: "Nome de usuário ou senha incorreta" })
+            }
+            else if (results[0].status === 'false') {
+                return res.status(401).json({ message: "Espere a aprovação do Admin" })
+            }
+            else if (results[0].password == user.password) {
+                const response = { email: results[0].email, role: results[0].role }
+                const accessToken = jwt.sign(response, process.env.ACCESS_TOKEN, { expiresIn: '8h' });
+                res.status(200).json({ token: accessToken })
+
+            }
+            else {
+                return res.status(400).json({ message: "Algo deu errado. Tente novamente mais tarde" })
+            }
+        }
+        else {
+            return res.status(500).json(err);
+        }
+    })
+})
 
 module.exports = router;
