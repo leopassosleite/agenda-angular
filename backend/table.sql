f5c9e7abd243eb9cbcfd2297c9e6180390e6e44a
leopassosleite <leopassos.leite4@gmail.com>
2022-05-20 08:41:02 -0300

    Implementa categorias

--- backend/table.sql
+++ backend/table.sql
@@ -1,12 +1,18 @@
 create table user(
     id int primary key AUTO_INCREMENT,
-    name varchar(250),
-    contactNumber varchar(20),
-    email varchar(50),
-    password varchar(250),
-    status varchar(250),
-    role varchar(20),
+    name varchar(250) NOT NULL,
+    contactNumber varchar(20) NOT NULL,
+    email varchar(50) NOT NULL,
+    password varchar(250) NOT NULL,
+    status varchar(250) NOT NULL,
+    role varchar(20) NOT NULL,
     UNIQUE(email)
 );
 
 insert into user(name,contactNumber,email,password,status,role) values ('admin','(51) 99154-6743','leopassos.leite@gmail.com','admin','true','admin');
+
+create table category(
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(3) NOT NULL,
+    primary key (id)
+);
